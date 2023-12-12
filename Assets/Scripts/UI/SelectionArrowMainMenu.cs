98f7efa2626535c3bb5f72e8c190d23719ce3069
konstantinosKatsamis <75335809+konstantinosKatsamis@users.noreply.github.com>
2023-12-12 19:31:58 +0200

    ui already setup and creation of arrow script

---
+++ Assets/Scripts/UI/SelectionArrowMainMenu.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectionArrowMainMenu : MonoBehaviour
+{
+    private RectTransform rect;
+
+    private void Awake()
+    {
+        //rect = 
+    }
+}
