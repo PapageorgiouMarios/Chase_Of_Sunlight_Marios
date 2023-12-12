a3314edab757cb40d401384d07eb4ae0125959c4
konstantinosKatsamis <75335809+konstantinosKatsamis@users.noreply.github.com>
2023-12-12 22:15:18 +0200

    end scene appears for 20sec

--- Assets/Scripts/EndScreen/EndScreenManager.cs
+++ Assets/Scripts/EndScreen/EndScreenManager.cs
@@ -1,11 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EndScreenManager : MonoBehaviour
 {
-    [SerializeField] private GameObject endScreen;
-    [SerializeField] private AudioClip endScreenSound;
+    
+    [SerializeField] private float duration = 20f;
+    private float timer = 0f;
 
-    //public void 
+    private void Start()
+    {
+        Debug.Log(" ----------------------------- Hello from endscene");
+        timer = 0f;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+            // Increment the timer
+            timer += Time.deltaTime;
+
+            // If the timer exceeds the duration, switch back to the first scene
+            if (timer >= duration)
+            {
+                SceneManager.LoadScene(0);
+                timer = 0f;
+            }
+        
+    }
+
 }
